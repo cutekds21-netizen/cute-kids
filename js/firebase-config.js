a7f5fcd7216cc6522cd08db0947ac98783d79998
cutekds21-netizen <cutekds21@gmail.com>
2026-08-22 02:15:47 +0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,10 +10,10 @@
 // ------------------------------------------------------------------
 
 window.FIREBASE_CONFIG = {
- apiKey: "AIzaSyDhbJ0SYdmhJWHcMvvBPIm8Yl9uh4PDCHk",
+  apiKey: "AIzaSyDhbJ0SYdmhJWHcMvvBPIm8Yl9uh4PDCHk",
   authDomain: "cute-kids-48c5a.firebaseapp.com",
   projectId: "cute-kids-48c5a",
   storageBucket: "cute-kids-48c5a.firebasestorage.app",
   messagingSenderId: "643261840017",
-  appId: "1:643261840017:web:1be895bad096ab50139d30"
+  appId: "1:643261840017:web:1be895bad096ab50139d30",
 };
